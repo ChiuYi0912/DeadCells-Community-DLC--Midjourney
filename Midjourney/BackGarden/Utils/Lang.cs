828046f7659c851ddd6a71cb33abc5973b068101
ChiuYi <3360625360@qq.com>
2026-02-16 00:27:19 +0800

    test

--- Midjourney/BackGarden/Utils/Lang.cs
+++ Midjourney/BackGarden/Utils/Lang.cs
@@ -8,6 +8,7 @@ namespace BackGarden.Utils.Lang
     {
         public BackGardenLang(BackGardenEntry entry)
         {
+
             EventSystem.AddReceiver(this);
             ModCore.Modules.GetText.Instance.RegisterMod("BackGardenLang");
             entry.Logger.Information("\x1b[34m Language Module Loading]\x1b[0m");
